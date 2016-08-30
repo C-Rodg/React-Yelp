7e942d7a58b00f5b211b8045e9314fe34d225853
Kobaine <rodgersbc@gmail.com>
2016-08-29 21:53:19 -0700

    Added react routes

--- src/containers/App/App.spec.js
+++ src/containers/App/App.spec.js
@@ -1,18 +1,17 @@
-import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 import App from './App';
 import styles from './styles.module.css';
 
-describe('<App />', function() {
+describe('<App />', () => {
 	let wrapper;
 	beforeEach(() => {
 		wrapper = shallow(<App />);
 	});
 
-	it('has a single wrapper element', () => {
-		expect(wrapper.find(`.${styles.wrapper}`))
+	it('has a Router component', () => {
+		expect(wrapper.find('Router'))
 			.to.have.length(1);
 	});
 });
